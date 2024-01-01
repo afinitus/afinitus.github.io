3b6ccff825294024f86a87fa734ed1991719c408
afinitus <98845821+afinitus@users.noreply.github.com>
2023-12-31 20:53:41 -0800

    all files
    just a lil thing

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('loveLetterButton').addEventListener('click', function() {
+    window.location.href = 'message.html'; // Replace with the actual path to your essay page
+});
